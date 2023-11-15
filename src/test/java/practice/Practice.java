d6a01c612541680138d7d9591e389ebd5a0fe005
Administrator <Administrator@DESKTOP-FI1EJ2S>
2023-11-15 11:47:26 +0530

    commit and push

---
+++ src/test/java/practice/Practice.java
@@ -0,0 +1,13 @@
+package practice;
+/*
+ * this is for commit and push practice 
+ */
+public class Practice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi");
+
+	}
+
+}
